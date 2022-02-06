2100fe9ba42ec3327b7749e64ba6d11cba61875f
ayanrajput <ayanrajput1234@gmail.com>
2022-02-06 23:24:09 +0530

    added function to print the board

--- game.py
+++ game.py
@@ -6,3 +6,31 @@ dimension=4
 # winning number 
 winning_number=2048
 
+# board
+board=[[0 for i in range(dimension)]for j in range(dimension)]
+
+def displayBoard():
+    # maximum value in the board
+    max_val=0
+    for r in board:
+        for c in r:
+            if c>max_val:
+                max_val=c
+
+    # spaces required
+    spacesRequired=len(str(max_val))
+
+    # now printing the board
+    for r in board:
+        line="|"
+        for c in r:
+            if c==0:
+                line+=" "*spacesRequired+"|"
+            else:
+                line+=" "*(spacesRequired-len(str(c)))+str(c)
+        print(line)
+
+
+
+
+    
